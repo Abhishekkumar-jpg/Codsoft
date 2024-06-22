d8ae17dcc333a82755d4224c395105f97402cae2
Abhishekkumar-jpg <abhishekkumar886055@gmail.com>
2024-06-22 23:33:41 +0530

    Update Numberguessinggame.java

--- Numberguessinggame.java
+++ Numberguessinggame.java
@@ -6,7 +6,7 @@ public class NumberGuessingGame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         
-        int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
+        int numberToGuess = random.nextInt(100) + 1; l
         int numberOfTries = 0;
         boolean hasGuessedCorrectly = false;
         
